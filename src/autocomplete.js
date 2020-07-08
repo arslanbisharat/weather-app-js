c5710ec2b5eb81058587f3f0dc3a7fcc0bb5412f
arslanbisharat <thearslan380@gmail.com>
2020-07-08 16:32:51 -0700

    Update autocomplete.js

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,8 +1,8 @@
 // PLACES
 export const placesAutocomplete = places({
-  appId: "plD80CGT2YTX",
-  apiKey: "ea3585946e68c7bffe43e74f97d73b0c",
-  container: document.querySelector("#cityNameInput"),
-}).configure({ type: "city" });
+  appId: 'plD80CGT2YTX',
+  apiKey: 'ea3585946e68c7bffe43e74f97d73b0c',
+  container: document.querySelector('#cityNameInput'),
+}).configure({ type: 'city' });
 
 console.log(placesAutocomplete.city);
