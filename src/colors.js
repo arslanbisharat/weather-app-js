02687e9ea222217e74a587e8ef90ad375e6194d8
arslanbisharat <thearslan380@gmail.com>
2020-07-08 10:00:25 -0700

    linters

--- src/colors.js
+++ src/colors.js
@@ -1,10 +1,10 @@
-export let colors = {
-  Clouds: "hsla(170, 39.2%, 52.2%, 0.5)",
-  Snow: "hsla(288, 39.2%, 77.2%, 0.7)",
-  Rain: "hsla(197, 75%, 25%, 0.7)",
-  Clear: "hsla(197, 39.2%, 52%, 0.7)",
-  Windy: "hsla(157, 39.2%, 52%, 0.6)",
-  Thunderstorm: "hsla(216, 39.2%, 0, 0.7)",
-  Drizzle: "hsla(198.3, 39.2%, 52%, 0.7)",
-  Sand: "hsla(360, 39.2%, 52%, 0.7)",
+export const colors = {
+  Clouds: 'hsla(170, 39.2%, 52.2%, 0.5)',
+  Snow: 'hsla(288, 39.2%, 77.2%, 0.7)',
+  Rain: 'hsla(197, 75%, 25%, 0.7)',
+  Clear: 'hsla(197, 39.2%, 52%, 0.7)',
+  Windy: 'hsla(157, 39.2%, 52%, 0.6)',
+  Thunderstorm: 'hsla(216, 39.2%, 0, 0.7)',
+  Drizzle: 'hsla(198.3, 39.2%, 52%, 0.7)',
+  Sand: 'hsla(360, 39.2%, 52%, 0.7)',
 };
