83ad712d8292e6830de32fe57f05a990a4ebfc14
Muhammad Arslan <thearslan380@gmail.com>
2020-07-08 15:45:52 +0500

    TSE comment
    Co-authored-by: Saheed Oladele <sdrfngg640@gmail.com>

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,5 +1,5 @@
 // PLACES
-export let placesAutocomplete = places({
+export const placesAutocomplete = places({
   appId: "plD80CGT2YTX",
   apiKey: "ea3585946e68c7bffe43e74f97d73b0c",
   container: document.querySelector("#cityNameInput"),
