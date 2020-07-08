b3d5896005e22caf92bd0765efd136196e1bb4f0
arslanbisharat <thearslan380@gmail.com>
2020-07-08 16:58:55 -0700

    Revert "Update autocomplete.js"
    This reverts commit 60f8d655e7c50c5a46c42a8eec4eaa5174bb73e1.

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,8 +1,6 @@
 // PLACES
-const placesAutocomplete = places({
+export const placesAutocomplete = places({
   appId: 'plD80CGT2YTX',
   apiKey: 'ea3585946e68c7bffe43e74f97d73b0c',
   container: document.querySelector('#cityNameInput'),
 }).configure({ type: 'city' });
-
-export default placesAutocomplete
