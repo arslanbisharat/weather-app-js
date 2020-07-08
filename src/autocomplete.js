60f8d655e7c50c5a46c42a8eec4eaa5174bb73e1
arslanbisharat <thearslan380@gmail.com>
2020-07-08 16:53:56 -0700

    Update autocomplete.js

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,6 +1,8 @@
 // PLACES
-export const placesAutocomplete = places({
+const placesAutocomplete = places({
   appId: 'plD80CGT2YTX',
   apiKey: 'ea3585946e68c7bffe43e74f97d73b0c',
   container: document.querySelector('#cityNameInput'),
 }).configure({ type: 'city' });
+
+export default placesAutocomplete
