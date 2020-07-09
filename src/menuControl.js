bd7abd43c4960d708df314f4bad66ca312d15825
arslanbisharat <thearslan380@gmail.com>
2020-07-08 17:44:32 -0700

    Update menuControl.js

--- src/menuControl.js
+++ src/menuControl.js
@@ -1,7 +1,6 @@
 //  adds a transition from the left with a cubic bezier easing function
 export default function menuControl(closeButtonID, openButtonID, menuID) {
   function menuToggle() {
-    console.info('Inside closeMenu');
     const menu = document.querySelector(`#${menuID}`);
     menu.classList.toggle('visible');
     menu.classList.toggle('hidden');
