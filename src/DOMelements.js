83e7220d505e480f3e8d5e635db28658cdb274dc
arslanbisharat <thearslan380@gmail.com>
2020-07-08 22:08:41 -0700

    Update DOMelements.js

--- src/DOMelements.js
+++ src/DOMelements.js
@@ -1,21 +1,21 @@
 import { fetchWeatherAsync, fetchFlickrPhoto, toggleLoader } from './index.js';
 import { getCountryCode } from './countryCode.js';
 // search button and array of divs with data
-let button = document.querySelector('#citySearchButton');
-export let dataDivs = [...document.querySelectorAll('.data')];
+const button = document.querySelector('#citySearchButton');
+export const dataDivs = [...document.querySelectorAll('.data')];
 
 // adds search button event handler
 button.addEventListener('click', () => {
   toggleLoader();
-  let searchQuery = document.querySelector('#cityNameInput').value;
-  let infoArray = searchQuery.split(',');
-  let nameOfCity = infoArray[0];
+  const searchQuery = document.querySelector('#cityNameInput').value;
+  const infoArray = searchQuery.split(',');
+  const nameOfCity = infoArray[0];
   // city name without the space in front of the name
-  let nameOfCountry = infoArray[infoArray.length - 1].replace(' ', '');
-  let countryCode = getCountryCode(nameOfCountry);
-  let query = `${nameOfCity},${countryCode}`;
+  const nameOfCountry = infoArray[infoArray.length - 1].replace(' ', '');
+  const countryCode = getCountryCode(nameOfCountry);
+  const query = `${nameOfCity},${countryCode}`;
 
-  let units = getUnit();
+  const units = getUnit();
 
   //clear search
   document.querySelector('#cityNameInput').value = '';
@@ -27,13 +27,13 @@ button.addEventListener('click', () => {
 });
 
 // celcius
-let metricButton = document.querySelector('.metric');
+const metricButton = document.querySelector('.metric');
 // fahrenheit
-let imperialButton = document.querySelector('.imperial');
-let unitsButtons = [metricButton, imperialButton];
+const imperialButton = document.querySelector('.imperial');
+const unitsButtons = [metricButton, imperialButton];
 
 function getUnit() {
-  let units = unitsButtons.filter((input) => {
+  const units = unitsButtons.filter((input) => {
     if (input.checked) {
       return input;
     }
@@ -48,8 +48,8 @@ unitsButtons.forEach((input) =>
 
 // this should be refactored to do maths on the unit data as opposed to fetching new data
 function switchUnits(e) {
-  let searchQuery = document.querySelector('#cityNameInput').value;
-  let units = e.target.value;
+  const searchQuery = document.querySelector('#cityNameInput').value;
+  const units = e.target.value;
   if (searchQuery !== '') {
     resetData(dataDivs);
     fetchWeatherAsync(searchQuery, units);
@@ -63,22 +63,22 @@ function menuToggle(menuID) {
   menu.classList.toggle('hidden');
 }
 
-export let name = document.querySelector('.cityName');
-export let country = document.querySelector('.country');
-export let date = document.querySelector('.weatherDate');
-export let descr = document.querySelector('.description');
-export let icon = document.querySelector('.weather-icon');
-export let weathName = document.querySelector('.weatherName');
-export let feelsLike = document.querySelector('.feels_like');
-export let humid = document.querySelector('.humidity');
-export let press = document.querySelector('.pressure');
-export let wind = document.querySelector('.wind');
-export let temp = document.querySelector('.temp');
-export let tempMax = document.querySelector('.temp_max');
-export let tempMin = document.querySelector('.temp_min');
-export let sunrise = document.querySelector('.sunrise');
-export let sunset = document.querySelector('.sunset');
+export const name = document.querySelector('.cityName');
+export const country = document.querySelector('.country');
+export const date = document.querySelector('.weatherDate');
+export const descr = document.querySelector('.description');
+export const icon = document.querySelector('.weather-icon');
+export const weathName = document.querySelector('.weatherName');
+export const feelsLike = document.querySelector('.feels_like');
+export const humid = document.querySelector('.humidity');
+export const press = document.querySelector('.pressure');
+export const wind = document.querySelector('.wind');
+export const temp = document.querySelector('.temp');
+export const tempMax = document.querySelector('.temp_max');
+export const tempMin = document.querySelector('.temp_min');
+export const sunrise = document.querySelector('.sunrise');
+export const sunset = document.querySelector('.sunset');
 
 // hero container
-export let hero = document.querySelector('.hero-container');
-export let heroImgContainer = document.querySelector('.hero-img-container');
+export const hero = document.querySelector('.hero-container');
+export const heroImgContainer = document.querySelector('.hero-img-container');
